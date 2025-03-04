848d878dbafb3d3f250fc08cb411bd7442706a6f
Bruno Ribeiro da Silva <brunoqkz@gmail.com>
2025-03-04 12:58:37 -0500

    feat: add Azure Blob Storage integration
    - Added dotenv configuration to load environment variables.
    - Included new route for handling Azure Blob Storage operations.
    - Updated .gitignore to include .env file.
    - Added new dependencies: @azure/storage-blob and dotenv.
    - Added new dev script using nodemon for development.

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+var blobRouter = require("./routes/blob");
 
 var app = express();
 
@@ -17,6 +18,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/blob", blobRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
