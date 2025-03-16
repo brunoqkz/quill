d33aa78cc78d47cd39d5b354bcd93af3c18894dd
Diego <bastosdiegol@gmail.com>
2025-03-15 21:59:32 -0400

    feat: author self-registration back-end endpoint

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,7 @@ var logger = require("morgan");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var blobRouter = require("./routes/blob");
+var authRouter = require("./routes/auth");
 
 var app = express();
 
@@ -19,6 +20,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/blob", blobRouter);
+app.use("/api/auth", authRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
