dede85e94eaa5cbd3a0d217ae1e79f5fb890e589
Diego <bastosdiegol@gmail.com>
2025-03-18 01:14:22 -0400

    feat: add Dashboard and NotFoundPage initial components

---
+++ frontend/src/components/NotFoundPage/index.jsx
@@ -0,0 +1,16 @@
+import "./style.scss";
+
+/**
+ * NotFoundPage component
+ * @returns {JSX.Element}
+ */
+function NotFoundPage() {
+  return (
+    <section className="flex flex-col items-center justify-center h-full">
+      <h1 className="font-bold">404 - Page Not Found</h1>
+      <p>The page you are looking for does not exist.</p>
+    </section>
+  );
+}
+
+export default NotFoundPage;
