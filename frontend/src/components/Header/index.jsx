7f1288c9eaf1023c9e1b0edfe43d49c401b3ee9c
Diego <bastosdiegol@gmail.com>
2025-03-18 01:24:58 -0400

    feat: Header component dynamically handling logout

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -1,7 +1,17 @@
 import "./style.scss";
 import logo from "../../assets/quill_logo_navy.png";
+import { useAuth } from "../AuthProvider";
+import { useNavigate } from "react-router-dom";
 
 function Header() {
+  const { user, logout } = useAuth();
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    await logout();
+    navigate("/");
+  };
+
   return (
     <header className="flex flex-col md:flex-row justify-between items-center text-center text-2xl">
       <div className="flex flex-1">
@@ -11,9 +21,13 @@ function Header() {
         <h1 className="font-bold uppercase">Ethereal Ink</h1>
       </div>
       <div className="flex flex-1 justify-end">
-        <button className="text-white" type="submit">
-          Book With Us
-        </button>
+        {user ? (
+          <button className="text-white" onClick={handleLogout}>
+            Logout
+          </button>
+        ) : (
+          <button className="text-white">Book With Us</button>
+        )}
       </div>
     </header>
   );
