1fbd523d01c5aaf49b9a70f93e14d0907744d0af
Diego <bastosdiegol@gmail.com>
2025-03-18 02:20:55 -0400

    chore: add cors dependency and enable localhost:5173 requests

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const cors = require("cors");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -16,6 +17,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
+// Enable CORS for localhost:5173
+app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
