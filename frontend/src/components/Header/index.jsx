36c03bcec070d2a89037817523cc6a14cd79a5cf
Diego <bastosdiegol@gmail.com>
2025-03-17 20:58:27 -0400

    style: update Header style

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -4,11 +4,17 @@ import logo from "../../assets/quill_logo_navy.png";
 function Header() {
   return (
     <header className="flex flex-col md:flex-row justify-between items-center text-center text-2xl">
-      <img src={logo} alt="Ethereal Ink" />
-      <h1 className="font-bold uppercase">Ethereal Ink</h1>
-      <button className="text-white" type="submit">
-        Book With Us
-      </button>
+      <div className="flex flex-1">
+        <img src={logo} alt="Ethereal Ink" />
+      </div>
+      <div className="flex flex-1 justify-center">
+        <h1 className="font-bold uppercase">Ethereal Ink</h1>
+      </div>
+      <div className="flex flex-1 justify-end">
+        <button className="text-white" type="submit">
+          Book With Us
+        </button>
+      </div>
     </header>
   );
 }
