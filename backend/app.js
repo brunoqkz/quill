6321739f2b0a99e43d97ff5eddf763ebbbaf3e1f
Bruno Ribeiro da Silva <brunoqkz@gmail.com>
2025-03-19 20:10:08 -0400

    refactor: improved app.js
    # Conflicts:
    #	backend/app.js

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,21 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+// Import required modules
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 const cors = require("cors");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-var blobRouter = require("./routes/blob");
-var authRouter = require("./routes/auth");
+// Import routes
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const blobRouter = require("./routes/blob");
+const authRouter = require("./routes/auth");
 
-var app = express();
+// Initialize Express app
+const app = express();
 
+// Configure middleware
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -27,22 +31,22 @@ app.use(
 );
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use("/api/users", usersRouter);
 app.use("/blob", blobRouter);
 app.use("/api/auth", authRouter);
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
+// 404 error handler
+app.use((req, res, next) => {
   next(createError(404));
 });
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
+// Global error handler
+app.use((err, req, res, next) => {
+  // Set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // respond with error
+  // Respond with error
   res.status(err.status || 500).json({
     error: err.message,
     details: req.app.get("env") === "development" ? err : {},
